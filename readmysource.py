7514f546abb75408da480491d8b5b25ba08c4a75
THYemre <selimemre4615-@hotmail.com>
2018-12-22 22:53:47 +0300

    Add files via upload

---
+++ readmysource.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python2.7
+print '''
+DEVELOPED BY THYemre
+'''
+import random
+
+impass = [0x53,0x47,0x46,0x6f,0x59,0x57,0x68,0x68,0x49,0x45,0x52,0x76,0x62,0x6e,0x51,0x67,0x56,0x48,0x4a,0x31,0x63,0x33,0x51,0x67,0x51,0x53,0x42,0x49,0x59,0x57,0x4e,0x72,0x5a,0x58,0x49,0x3d]
+kkbro = '\x54\x48'
+tkumawfk = []
+password = [0x73,0x79,0x73,0x61,0x64,0x6d,0x69,0x6e,0x21,0x21]
+sysadminpassword = [0x63,0x33,0x6c,0x7a,0x59,0x57,0x52,0x74,0x61,0x57,0x34,0x68,0x49,0x51,0x3d,0x3d]
+goon = '\x59\x65\x6d'
+mainvalue = random.randint(0,9381)
+canthackme = random.randint(0,1000)
+hackmeplease = canthackme * 2
+dontgiveup = '\x72\x65'
+hackmeplease = hackmeplease + 20
+warninginhere = [0x69,0x73,0x53,0x79,0x73,0x61,0x64,0x6d,0x69,0x6e]
+hackmeplease = hackmeplease/2
+mytoken = "94a08da1fecbb6e8b46990538c7b50b2"
+adminPassword = []
+hackmeplease = hackmeplease - canthackme
+hackmeplease = hackmeplease ^ 3
+donttrustme = kkbro + goon + dontgiveup
+mainarray = []
+
+print 'Did you read the source carefully? -THYemre'
+print 'Add this to username: ' + str(mainvalue)
+for key in sysadminpassword:
+	enckey = hackmeplease ^ key
+	tkumawfk.append(enckey)
+for give in warninginhere:
+	passkey = hackmeplease ^ give
+	mainarray.append(passkey)
+
+unm = raw_input("What is the username: \n")
+if unm == donttrustme:
+	pass
+else:
+	print "Everything is in the source code !!!"
+	exit(0)
+for decodeMe in sysadminpassword:
+	decoder = mainvalue ^ decodeMe
+	adminPassword.append(decoder)
+print "Well done !"
+passinherepls = raw_input("What is the password: ")
+for passcheck in passinherepls:
+	for char in tkumawfk:
+		if passcheck == str(chr(char)):
+			print 'You are not script kiddie !! Well Done !'
+			print 'Your last mission : Crack the encrypted text and send us.That in below'
+			print '\n\n'
+			print 'Tm93IENyYWNrIFRoZW06Ck9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2shIE9vaz8gT29rLiBPb2s/IE9vay4gT29rLiBPb2suIE9vaz8gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2s/IE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vaz8gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vaz8gT29rLiBPb2s/IE9vay4gT29rPyBPb2suIE9vaz8gT29rLiBPb2shIE9vayEgT29rPyBPb2shIE9vay4gT29rPyBPb2suIE9vaz8gT29rLiBPb2s/IE9vay4gT29rPyBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rISBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vayEgT29rLiBPb2s/IE9vay4gT29rPyBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rISBPb2suIE9vay4gT29rPyBPb2suIE9vay4gT29rISBPb2suIE9vay4gT29rPyBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2suIE9vay4gT29rLiBPb2shIE9vay4gT29rPyBPb2suIE9vaz8gT29rLiBPb2shIE9vay4gT29rLiBPb2s/IE9vay4gT29rLiBPb2suIE9vay4gT29rISBPb2suIE9vay4gT29rPyBPb2shIE9vay4gT29rPyBPb2suIE9vaz8gT29rLiBPb2shIE9vay4gT29rLiBPb2s/IE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rLiBPb2suIE9vaz8gT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vay4gT29rISBPb2suIE9vayEgT29rISBPb2shIE9vayEgT29rISBPb2shIE9vayEgT29rLiBPb2s/IE9vay4gT29rPyBPb2suIE9vay4gT29rLiBPb2shIE9vay4g'
+			break
+		else:
+			print 'Dont be script kiddie and develop script for solve this ! Or read source !'
+			exit(0)
+	break
